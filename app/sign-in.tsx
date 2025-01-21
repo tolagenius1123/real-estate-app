83974ba3415f5bcac903e2f15a599daad478b939
Omotola Jinadu <91120863+tolagenius1123@users.noreply.github.com>
2025-01-21 05:22:40 +0100

    implemented profile screen

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,40 +1,73 @@
-import {View, Text, Image, ScrollView, TouchableOpacity} from "react-native";
-import {SafeAreaView} from "react-native-safe-area-context";
+import {
+	View,
+	Text,
+	Image,
+	ScrollView,
+	TouchableOpacity,
+	Alert,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import images from "@/constants/images";
 import icons from "@/constants/icons";
+import { login } from "@/lib/appwrite";
+import { useGlobalContext } from "@/lib/global-provider";
+import { Redirect } from "expo-router";
 
 const SignIn = () => {
-    const handleLogin = () => {
-    };
-    return (
-        <SafeAreaView className="bg-white h-full">
-            <ScrollView contentContainerClassName="h-full">
-                <Image source={images.onboarding} className="w-full h-4/6 " resizeMode="contain"/>
-                <View className="px-10">
-                    <Text className="text-base text-center uppercase font-rubik text-black-200">Welcome to
-                        ReState
-                    </Text>
-                    <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
-                        Let's Get You Closer to {"\n"}
-                        <Text className="text-primary-300">Your Ideal Home</Text>
-                    </Text>
-                    <Text className="text-lg font-rubik text-black-200 text-center mt-12">
-                        Login to ReState with Google
-                    </Text>
-                    <TouchableOpacity
-                        className="bg-white border border-zinc-300 shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5"
-                        onPress={handleLogin}
-                    >
-                        <View className="flex flex-row items-center justify-center gap-2">
-                            <Image source={icons.google} className="w-5 h-5" resizeMode="contain"/>
-                            <Text className="text-lg font-rubik-medium text-black-300">Continue with Google or
-                                Email</Text>
-                        </View>
-                    </TouchableOpacity>
-                </View>
-            </ScrollView>
-        </SafeAreaView>
-    )
-}
+	const { loading, refetch, isLoggedIn } = useGlobalContext();
+
+	if (!loading && isLoggedIn) return <Redirect href="/" />;
+
+	const handleLogin = async () => {
+		const result = await login();
+
+		if (result) {
+			refetch();
+		} else {
+			Alert.alert("Error", "Failed to login");
+		}
+	};
+
+	return (
+		<SafeAreaView className="bg-white h-full">
+			<ScrollView contentContainerClassName="h-full">
+				<Image
+					source={images.onboarding}
+					className="w-full h-4/6 "
+					resizeMode="contain"
+				/>
+				<View className="px-10">
+					<Text className="text-base text-center uppercase font-rubik text-black-200">
+						Welcome to ReState
+					</Text>
+					<Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
+						Let's Get You Closer to {"\n"}
+						<Text className="text-primary-300">
+							Your Ideal Home
+						</Text>
+					</Text>
+					<Text className="text-lg font-rubik text-black-200 text-center mt-12">
+						Login to ReState with Google
+					</Text>
+					<TouchableOpacity
+						className="bg-white border border-zinc-300 shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5"
+						onPress={handleLogin}
+					>
+						<View className="flex flex-row items-center justify-center gap-2">
+							<Image
+								source={icons.google}
+								className="w-5 h-5"
+								resizeMode="contain"
+							/>
+							<Text className="text-lg font-rubik-medium text-black-300">
+								Continue with Google
+							</Text>
+						</View>
+					</TouchableOpacity>
+				</View>
+			</ScrollView>
+		</SafeAreaView>
+	);
+};
 
 export default SignIn;
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,83 +1,84 @@
-import {Account, Avatars, Client, OAuthProvider} from "react-native-appwrite";
+import { Account, Avatars, Client, OAuthProvider } from "react-native-appwrite";
 import * as Linking from "expo-linking";
-import {openAuthSessionAsync} from "expo-web-browser";
+import { openAuthSessionAsync } from "expo-web-browser";
 
 export const config = {
-    platform: 'com.jsm.restate',
-    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
-    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
-}
+	platform: "com.jsm.restate",
+	endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+	projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+};
 
 export const client = new Client();
 
 client
-    .setEndpoint(config.endpoint!)
-    .setProject(config.projectId!)
-    .setPlatform(config.platform!)
+	.setEndpoint(config.endpoint!)
+	.setProject(config.projectId!)
+	.setPlatform(config.platform!);
 
 export const avatar = new Avatars(client);
 export const account = new Account(client);
 
 export async function login() {
-    try {
-        const redirectUri = Linking.createURL('/');
+	try {
+		const redirectUri = Linking.createURL("/");
 
-        const response = await account.createOAuth2Token(
-            OAuthProvider.Google,
-            redirectUri
-        );
+		const response = await account.createOAuth2Token(
+			OAuthProvider.Google,
+			redirectUri
+		);
 
-        if (!response) throw new Error("Failed to login");
+		if (!response) throw new Error("Failed to login");
 
-        const browserResult = await openAuthSessionAsync(
-            response.toString(),
-            redirectUri
-        );
+		const browserResult = await openAuthSessionAsync(
+			response.toString(),
+			redirectUri
+		);
 
-        if (browserResult.type !== 'success') throw new Error("Failed to login");
+		if (browserResult.type !== "success")
+			throw new Error("Failed to login");
 
-        const url = new URL(browserResult.url);
+		const url = new URL(browserResult.url);
 
-        const secret = url.searchParams.get("secret")?.toString();
-        const userId = url.searchParams.get("userId")?.toString();
+		const secret = url.searchParams.get("secret")?.toString();
+		const userId = url.searchParams.get("userId")?.toString();
 
-        if (!secret || !userId) throw new Error("Failed to login");
+		if (!secret || !userId) throw new Error("Failed to login");
 
-        const session = await account.createSession(userId, secret);
+		const session = await account.createSession(userId, secret);
 
-        if (!session) throw new Error("Failed to create a session");
+		if (!session) throw new Error("Failed to create a session");
 
-        return true;
-    } catch (e) {
-        console.error(e);
-        return false;
-    }
+		return true;
+	} catch (e) {
+		console.error(e);
+		return false;
+	}
 }
 
 export async function logout() {
-    try {
-        await account.deleteSession('current');
-        return true;
-    } catch (e) {
-        console.error(e);
-        return false;
-    }
+	try {
+		await account.deleteSession("current");
+		return true;
+	} catch (e) {
+		console.error(e);
+		return false;
+	}
 }
 
-export async function getUser() {
-    try {
-        const response = await account.get();
+export async function getCurrentUser() {
+	try {
+		const response = await account.get();
 
-        if (response.$id) {
-            const useAvatar = avatar.getInitials(response.name);
+		if (response.$id) {
+			const useAvatar = avatar.getInitials(response.name);
 
-            return {
-                ...response,
-                avatar: useAvatar.toString(),
-            }
-        }
-    } catch (e) {
-        console.error(e);
-        return null;
-    }
+			return {
+				...response,
+				avatar: useAvatar.toString(),
+			};
+		}
+	} catch (e) {
+		console.error(e);
+		return null;
+	}
 }
